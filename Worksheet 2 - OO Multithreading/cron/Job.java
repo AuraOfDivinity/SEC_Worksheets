8adab3e5a13d99567497257510d33259b11b454a
Asel <peirisasel373@gmail.com>
2020-11-10 18:14:01 +0530

    Initial commit

---
+++ Worksheet 2 - OO Multithreading/cron/Job.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+/**
+ * Represents a job (command) to be run, and performs the execution. (You need to fill in the 
+ * details!)
+ */
+public class Job implements Runnable
+{
+    // ...
+}
---
+++ Worksheet 2 - OO Multithreading/cron/Scheduler.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/**
+ * The scheduler keeps track of all the jobs, and runs each one at the appropriate time. (You need
+ * to fill in the details!)
+ */
+public class Scheduler
+{
+    // ...
+
+    public void addJob(Job newJob)
+    {
+        // ...
+    }
+    
+    public void start()
+    {
+        // ...
+    }
+
+    public void stop()
+    {
+        // ...
+    }
+}
